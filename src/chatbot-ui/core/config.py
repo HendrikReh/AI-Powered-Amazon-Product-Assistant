cef0c5acf0a660e399d4105a3a1c1e935b091a52
Hendrik Reh <hendrik.reh@outlook.com>
2025-07-03 11:25:13 +0200

    add: ollama

--- src/chatbot-ui/core/config.py
+++ src/chatbot-ui/core/config.py
@@ -6,6 +6,7 @@ class Config(BaseSettings):
     GROQ_API_KEY: str
     GOOGLE_API_KEY: str
     WANDB_API_KEY: Optional[str] = None
+    OLLAMA_BASE_URL: Optional[str] = "http://localhost:11434"
 
     model_config = SettingsConfigDict(env_file=".env")
 
